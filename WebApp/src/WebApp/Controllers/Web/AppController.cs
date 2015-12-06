3fb1179985a2f1c416c4201223aa2bcddfdce0c3
amador-e-rivera <amador.e.rivera@gmail.com>
2015-12-06 15:28:39 -0800

    Added Contact page to send email with validation.

--- WebApp/src/WebApp/Controllers/Web/AppController.cs
+++ WebApp/src/WebApp/Controllers/Web/AppController.cs
@@ -1,11 +1,19 @@
 using Microsoft.AspNet.Mvc;
 using System;
-
+using WebApp.Services;
+using WebApp.ViewModels;
 
 namespace WebApp.Controllers.Web
 {
     public class AppController : Controller
     {
+        private IMailService _mailService;
+
+        public AppController(IMailService service)
+        {
+            _mailService = service;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -18,6 +26,18 @@ namespace WebApp.Controllers.Web
 
         public IActionResult Contact()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var email = Startup.Configuration["AppSettings:siteEmailAddress"];
+                _mailService.SendMail(email, email, $"Contact from {model.Name} ({model.Email})", model.Message);
+
+            }
             return View();
         }
     }
