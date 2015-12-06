d0070900169e2e60d65f0211eb71cd708820b7c6
amador-e-rivera <amador.e.rivera@gmail.com>
2015-12-06 12:01:16 -0800

    Added MVC6 dependency. Added App Controller and necessary views.

---
+++ WebApp/src/WebApp/Controllers/Web/AppController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNet.Mvc;
+using System;
+
+
+namespace WebApp.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
